d5ab3dc11696a01b901308ab0c3d552f9d4167cd
Anna Lyons <Anna.Lyons@nicta.com.au>
2016-06-22 11:41:28 +1000

    libjansson: port to seL4 build system

---
+++ libjansson/include/jansson_config.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#pragma once
+
+#include <autoconf.h>
+
+#define JSON_INLINE inline
+#define JSON_INTEGER_IS_LONG_LONG 0
+#define JSON_HAVE_LOCALECONV 0
+#define HAVE_STDINT_H 1
+
