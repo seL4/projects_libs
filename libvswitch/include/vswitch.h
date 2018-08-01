7842f517044d8edc26bb8293725272f250c9f680
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2018-08-01 15:40:50 +1000

    vswitch: Updated vswitch documentation
    Updated the interface documentation/comments.

--- libvswitch/include/vswitch.h
+++ libvswitch/include/vswitch.h
@@ -21,8 +21,11 @@
 
 #include <virtqueue.h>
 
-/* Need to introduce this variable into the build system */
+/* TODO: Need to introduce or paramterise this variable into the
+ * build system
+ */
 #define CONFIG_SEL4VSWITCH_NUM_NODES           (4)
+/* MAC address print format*/
 #define PR_MAC802_ADDR                      "%x:%x:%x:%x:%x:%x"
 /* Expects a *pointer* to a struct ether_addr */
 #define PR_MAC802_ADDR_ARGS(a)              (a)->ether_addr_octet[0], \
@@ -32,6 +35,10 @@
                                             (a)->ether_addr_octet[4], \
                                             (a)->ether_addr_octet[5]
 
+/*
+ * Represents the virtqueues used in a given link between
+ * two nodes.
+ */
 typedef struct vswitch_virtqueues_ {
     virtqueue_t *send_queue;
     virtqueue_t *recv_queue;
@@ -57,25 +64,15 @@ typedef struct vswitch_node_ {
     vswitch_virtqueues_t virtqueues;
 } vswitch_node_t;
 
-/* An instance of this structure will probably need to go into the shared
- * window that is shared between all the guests.
+/*
+ * Each component participating in a vswitch topology should have a
+ * MAC address assigned to them. It is expected during the initialisation of
+ * these nodes they will call "vswitch_connect()" to register the
+ * MAC address of the other nodes it is linked to. This will inturn
+ * populate the vswitch_t data structure.
  *
- * Since it will be written to by multiple guests, and since it is necessary for
- * the other guests to see the writes done by their peers, we must instruct the
- * compiler to commit all stores to memory: hence "volatile".
- *
- * Essentially, CAmkES should have assigned a MAC address to each guest VM.
- * Each of the Guest VMs' templates should call "vswitch_connect()" while it
- * is initializing itself to inform this library of what its MAC address is.
- *
- * Furthermore, each Guest VM will have a bufferqueue of some kind
- * (sel4bufferqueue?) which this library will have to call on to get a
- * pointer to a buffer to use as the incoming queue for each of the guest VMs
- * (->_allocate()).
- *
- * I expect that the locking for the _allocate() method will be done internally
- * to the sel4bufferqueue library, so I shouldn't need to have a lock for each
- * Guest VM's connection in here.
+ * Furthermore, each node is expected to have a send and recieve
+ * virtqueue for each destination node it has connection to.
  */
 typedef struct vswitch_ {
     int n_connected;
@@ -88,38 +85,38 @@ typedef struct vswitch_ {
  */
 int vswitch_init(vswitch_t *lib);
 
-/** Initializes metadata to track a Guest VM node connected to the VSWITCH
+/** Initializes metadata to track a destination node connected to the VSWITCH
  * bcast domain.
  *
- * During initialization of each Guest, each Guest is expected to call this
- * function to register its MAC address and the location of its queue of
- * buffers.
+ * During initialization of each component it is expected to
+ * call this function to register the MAC address and virtqueues of all
+ * the other nodes it is connected to.
  *
  * @param lib An initialized handle an instance of this library.
  * @param guest_macaddr A pointer to a mac address for the Guest VM being
  *                      registered.
- * @param guest_virtqueue An inialized handle to a virtqueue of some kind for
- *                        the Guest VM being registered. We will use this
- *                        virtqueue as the in-tray for new ethernet frames to be
- *                        delivered to the Guest VM being registered.
+ * @param send_virtqueue An initialized handle to a send virtqueue for
+ *                        the destination being registered.
+ * @param recv_virtqueue An initialized handle to a recieve virtqueue for
+ *                        the destination being registered.
  */
 int vswitch_connect(vswitch_t *lib,
                         struct ether_addr *guest_macaddr,
                         virtqueue_t *send_virtqueue,
                         virtqueue_t *recv_virtqueue);
 
-/** Checks to see if a Guest with the MAC address "mac" has been registered with
+/** Checks to see if a destination with the MAC address "mac" has been registered with
  * the library.
  *
  * @param lib Initialized instance of this library.
- * @param mac Mac address of the destination Guest to be looked up.
+ * @param mac Mac address of the destination to be looked up.
  * @return Positive integer if the specified MAC address has been registered.
  *         Negative integer if not.
  */
 int vswitch_get_destnode_index_by_macaddr(vswitch_t *lib,
                                               struct ether_addr *mac);
 
-/** Used to iterate through all the registered guests indiscriminately.
+/** Used to iterate through all the registered destinations indiscriminately.
  * @param lib Initialized instance of this library.
  * @param index Positive integer from 0 to CONFIG_SEL4VSWITCH_NUM_NODES.
  * @return NULL if an invalid index is supplied. Non-NULL if a valid index is
